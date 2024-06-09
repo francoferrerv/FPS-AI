334db5eea01d0750f95ae05a8d10be8b7b206571
Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>
2024-06-08 22:08:50 -0300

    refactor: improve separation of concerns between BaseNPCController and TalkingNPCController
    Signed-off-by: Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>

--- Assets/Scripts/BaseNPCController.cs
+++ Assets/Scripts/BaseNPCController.cs
@@ -6,15 +6,31 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.CrossPlatformInput;
 
+
+public enum NPCState
+{
+    Idle,
+    WalkingToChair,
+    TurningAroundChair,
+    SittingDown,
+    Sitting,
+    StandingUp
+}
+
 public class BaseNPCController : MonoBehaviour
 {
-
     protected internal NavMeshAgent agent;
     protected internal NPCHealth health;
     public float damageDistance;
     [SerializeField]
     protected internal Animator animator;
     protected internal int damagePerHit = 1;
+
+    private Vector3 chairPosition;
+    private Vector3 chairEulerAngles;
+    private Vector3 initialEulerAngles;
+    private float interpolationRatio;
+    protected NPCState state = NPCState.Idle;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +42,7 @@ public class BaseNPCController : MonoBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
+        UpdateState();
     }
 
     void FixedUpdate()
@@ -39,8 +56,26 @@ public class BaseNPCController : MonoBehaviour
         //animator.SetBool("Running", Input.GetKey(KeyCode.LeftShift));
     }
 
+    protected NPCState sitOnRandomChair()
+    {
+        GameObject chair = Chair.getRandomChair();
+
+        chairPosition = chair.transform.position + chair.transform.forward * 0.4f;
+        chairEulerAngles = chair.transform.rotation.eulerAngles;
+        agent.SetDestination(chairPosition);
+
+        return NPCState.WalkingToChair;
+    }
+
+    protected NPCState standUp()
+    {
+        animator.SetTrigger("IsStandingUp");
+
+        return NPCState.StandingUp;
+    }
+
     // Check if we've reached the destination
-    protected bool reachedDestination()
+    private bool reachedDestination()
     {
         if (!agent.pathPending)
         {
@@ -54,5 +89,100 @@ public class BaseNPCController : MonoBehaviour
         }
 
         return false;
+    }
+
+    private void UpdateState()
+    {
+        switch (state)
+        {
+            case NPCState.WalkingToChair:
+            {
+                state = walkToChair();
+                break;
+            }
+            case NPCState.TurningAroundChair:
+            {
+                state = turnAroundChair();
+                break;
+            }
+            case NPCState.SittingDown:
+            {
+                state = sittingDown();
+                break;
+            }
+            case NPCState.StandingUp:
+            {
+                state = standingUp();
+                break;
+            }
+        }
+    }
+
+    private NPCState walkToChair()
+    {
+        if (reachedDestination())
+        {
+            initialEulerAngles = transform.rotation.eulerAngles;
+            interpolationRatio = 0f;
+            animator.SetTrigger("isTurningAroundChair");
+
+            return NPCState.TurningAroundChair;
+        }
+
+        return NPCState.WalkingToChair;
+    }
+
+    private bool stillTurning()
+    {
+        return interpolationRatio < 1.0f;
+    }
+
+    private NPCState turnAroundChair()
+    {
+        float fromY = initialEulerAngles.y;
+        float toY = chairEulerAngles.y;
+
+        interpolationRatio += 10.0f / Mathf.Abs(toY-fromY);
+
+        float x = initialEulerAngles.x;
+        float y = Mathf.Lerp(fromY, toY, interpolationRatio);
+        float z = initialEulerAngles.z;
+
+        transform.rotation = Quaternion.Euler(x, y, z);
+
+        if (stillTurning())
+        {
+            return NPCState.TurningAroundChair;
+        }
+
+        animator.SetTrigger("IsSittingDown");
+
+        return NPCState.SittingDown;
+    }
+
+    private NPCState sittingDown()
+    {
+        bool isSittingDown = animator.GetBool("IsSittingDown");
+
+        if (isSittingDown)
+        {
+            return NPCState.SittingDown;
+        }
+
+        animator.SetTrigger("IsSitting");
+
+        return NPCState.Sitting;
+    }
+
+    private NPCState standingUp()
+    {
+        bool isStandingUp = animator.GetBool("IsStandingUp");
+
+        if (isStandingUp)
+        {
+            return NPCState.StandingUp;
+        }
+
+        return NPCState.Idle;
     }
 }
--- Assets/Scripts/TalkingNPCController.cs
+++ Assets/Scripts/TalkingNPCController.cs
@@ -6,24 +6,8 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.CrossPlatformInput;
 
-public enum ChairState
+public class TalkingNPCController: BaseNPCController
 {
-    Idle,
-    WalkingToChair,
-    TurningAroundChair,
-    SittingDown,
-    Sitting,
-    StandingUp
-}
-
-public class NPCMovement : BaseNPCController
-{
-    private Vector3 chairPosition;
-    private Vector3 chairEulerAngles;
-    private Vector3 initialNPCEulerAngles;
-    private float interpolationRatio;
-    private ChairState chairState = ChairState.Idle;
-
     protected override void Update()
     {
         base.Update();
@@ -38,124 +22,14 @@ public class NPCMovement : BaseNPCController
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            if (chairState == ChairState.Idle)
+            if (state == NPCState.Idle)
             {
-                chairState = startWalkingToChair();
+                state = sitOnRandomChair();
             }
-            else if (chairState == ChairState.Sitting)
+            else if (state == NPCState.Sitting)
             {
-                chairState = standUp();
-            }
-        }
-
-        switch (chairState)
-        {
-            case ChairState.WalkingToChair:
-            {
-                chairState = walkToChair();
-                break;
-            }
-            case ChairState.TurningAroundChair:
-            {
-                chairState = turnAroundChair();
-                break;
-            }
-            case ChairState.SittingDown:
-            {
-                chairState = sittingDown();
-                break;
-            }
-            case ChairState.StandingUp:
-            {
-                chairState = standingUp();
-                break;
+                state = standUp();
             }
         }
-    }
-
-    protected ChairState startWalkingToChair()
-    {
-        GameObject chair = Chair.getRandomChair();
-
-        chairPosition = chair.transform.position + chair.transform.forward * 0.4f;
-        chairEulerAngles = chair.transform.rotation.eulerAngles;
-        agent.SetDestination(chairPosition);
-
-        return ChairState.WalkingToChair;
-    }
-
-    protected ChairState walkToChair()
-    {
-        if (reachedDestination())
-        {
-            initialNPCEulerAngles = transform.rotation.eulerAngles;
-            interpolationRatio = 0f;
-            animator.SetTrigger("isTurningAroundChair");
-
-            return ChairState.TurningAroundChair;
-        }
-
-        return ChairState.WalkingToChair;
-    }
-
-    protected bool stillTurning()
-    {
-        return interpolationRatio < 1.0f;
-    }
-
-    protected ChairState turnAroundChair()
-    {
-        float fromY = initialNPCEulerAngles.y;
-        float toY = chairEulerAngles.y;
-
-        interpolationRatio += 10.0f / Mathf.Abs(toY-fromY);
-
-        float x = initialNPCEulerAngles.x;
-        float y = Mathf.Lerp(fromY, toY, interpolationRatio);
-        float z = initialNPCEulerAngles.z;
-
-        transform.rotation = Quaternion.Euler(x, y, z);
-
-        if (stillTurning())
-        {
-            return ChairState.TurningAroundChair;
-        }
-
-        animator.SetTrigger("IsSittingDown");
-
-        return ChairState.SittingDown;
-    }
-
-    protected ChairState sittingDown()
-    {
-        bool isSittingDown = animator.GetBool("IsSittingDown");
-
-        if (isSittingDown)
-        {
-            return ChairState.SittingDown;
-        }
-
-        animator.SetTrigger("IsSitting");
-
-        return ChairState.Sitting;
-    }
-
-    protected ChairState standUp()
-    {
-        animator.SetTrigger("IsStandingUp");
-
-        return ChairState.StandingUp;
-    }
-
-    protected ChairState standingUp()
-    {
-        bool isStandingUp = animator.GetBool("IsStandingUp");
-
-        if (isStandingUp)
-        {
-            return ChairState.StandingUp;
-        }
-
-        return ChairState.Idle;
     }
 }
