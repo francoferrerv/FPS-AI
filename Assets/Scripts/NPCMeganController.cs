c69ccbe7bfff594d581118cffe2b8d0226679875
Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>
2024-06-16 22:58:24 -0300

    feat: add TalkingNPCController script
    Signed-off-by: Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>

--- Assets/Scripts/NPCMeganController.cs
+++ Assets/Scripts/NPCMeganController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPCMeganController: NPCController
+public class NPCMeganController: TalkingNPCController
 {
     public GameObject player;
     private PlayerController playerController;
