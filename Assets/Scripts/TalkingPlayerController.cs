7b24b6ada6a716fad0b7c966c7ccd61707986a14
Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>
2024-06-16 22:58:01 -0300

    feat: add TalkingPlayerController script
    Signed-off-by: Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>

---
+++ Assets/Scripts/TalkingPlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TalkingPlayerController: PlayerController
+{
+    public override void TalkTo(ITalkingCharacter talkingCharacter)
+    {
+        Debug.Log($"{name}: I can talk to {talkingCharacter.name}.");
+    }
+
+    public override void StopTalkingTo(ITalkingCharacter talkingCharacter)
+    {
+        Debug.Log($"{name}: nobody wants to talk to me.");
+    }
+}
