88854f7a07b037d920afa414d4a7d473373de4a3
Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>
2024-06-17 21:19:27 -0300

    fix: invoke base class event handlers in TalkingPlayerController
    Signed-off-by: Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>

--- Assets/Scripts/TalkingPlayerController.cs
+++ Assets/Scripts/TalkingPlayerController.cs
@@ -7,11 +7,13 @@ public class TalkingPlayerController: PlayerController
 {
     public override void TalkTo(ITalkingCharacter talkingCharacter)
     {
+        base.TalkTo(talkingCharacter);
         Debug.Log($"{name}: I can talk to {talkingCharacter.name}.");
     }
 
     public override void StopTalkingTo(ITalkingCharacter talkingCharacter)
     {
+        base.StopTalkingTo(talkingCharacter);
         Debug.Log($"{name}: nobody wants to talk to me.");
     }
 }
