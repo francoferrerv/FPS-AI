7d08394ba912e138538e525eccfa8667663344a8
Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>
2024-06-17 21:20:40 -0300

    feat: activate Megan's virtual camera when talking to her
    Signed-off-by: Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>

--- Assets/Scripts/NPCMeganController.cs
+++ Assets/Scripts/NPCMeganController.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Cinemachine;
 
 public class NPCMeganController: TalkingNPCController
 {
     public GameObject player;
     private PlayerController playerController;
+    private CinemachineVirtualCamera camera;
 
     protected override void Start()
     {
         base.Start();
         playerController = player.GetComponent<PlayerController>();
+        GameObject gameObject = GameObject.Find("MeganVirtualCamera");
+        camera = gameObject.GetComponent<CinemachineVirtualCamera>();
         sitOnClosestSeat();
     }
 
@@ -23,5 +27,19 @@ public class NPCMeganController: TalkingNPCController
         }
 
         base.Update();
+    }
+
+    public override void TalkTo(ITalkingCharacter talkingCharacter)
+    {
+        base.TalkTo(talkingCharacter);
+        camera.Priority = 11;
+        camera.gameObject.SetActive(true);
+    }
+
+    public override void StopTalkingTo(ITalkingCharacter talkingCharacter)
+    {
+        base.StopTalkingTo(talkingCharacter);
+        camera.Priority = 0;
+        camera.gameObject.SetActive(false);
     }
 }
