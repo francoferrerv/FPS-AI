df540d4a57f1087524113730785d68a3c4c2cdc4
Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>
2024-07-19 22:22:40 -0300

    feat: allow player to switch between character models
    Signed-off-by: Sebastian Javier D'Alessandro Szymanowski <sdalessandro@fi.uba.ar>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,57 @@ public class PlayerController: BaseController
 {
     protected internal ThirdPersonController thirdPersonController;
     public bool paused { get; private set; }
-    public GameObject Leonard;
-    public GameObject Policeman;
+    public Avatar[] avatars;
+    public GameObject[] models;
+    protected int currentCharacter;
+    protected int characterCount;
 
     protected override void Start()
     {
         base.Start();
         thirdPersonController = this.GetComponent<ThirdPersonController>();
+        characterCount = avatars.Length;
+
+        for (int i = 0; i < characterCount; i++)
+        {
+            GameObject model = models[i];
+
+            if (model.activeSelf)
+            {
+                Debug.Log($"{model.name} is active");
+                currentCharacter = i;
+            }
+        }
     }
 
     protected override void Update()
     {
+        if (Input.GetKeyDown(KeyCode.PageUp))
+        {
+            disableCurrentCharacter();
+            currentCharacter--;
+
+            if (currentCharacter < 0)
+            {
+                currentCharacter = characterCount - 1;
+            }
+
+            enableCurrentCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.PageDown))
+        {
+            disableCurrentCharacter();
+            currentCharacter++;
+
+            if (currentCharacter > characterCount)
+            {
+                currentCharacter = 0;
+            }
+
+            enableCurrentCharacter();
+        }
+
         if (Input.GetKeyDown(KeyCode.P))
         {
             Pause();
@@ -54,6 +94,24 @@ public class PlayerController: BaseController
         }
 
         base.Update();
+    }
+
+    protected void enableCurrentCharacter()
+    {
+        Avatar avatar = avatars[currentCharacter];
+        GameObject model = models[currentCharacter];
+
+        Debug.Log($"enbling {model.name}...");
+        model.SetActive(true);
+        animator.avatar = avatar;
+    }
+
+    protected void disableCurrentCharacter()
+    {
+        GameObject model = models[currentCharacter];
+
+        Debug.Log($"disabling {model}...");
+        model.SetActive(false);
     }
 
     protected void Pause()
